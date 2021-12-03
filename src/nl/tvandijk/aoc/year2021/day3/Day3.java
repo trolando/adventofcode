6229772f6db85be38ea031bd960c768f74d40e9c
Tom van Dijk <tom@tvandijk.nl>
2021-12-03 07:58:18 +0100

    Day 3 solutions

---
+++ src/nl/tvandijk/aoc/year2021/day3/Day3.java
@@ -0,0 +1,93 @@
+package nl.tvandijk.aoc.year2021.day3;
+
+import nl.tvandijk.aoc.common.Day;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Day3 extends Day {
+
+    @Override
+    protected void part1(String fileContents) {
+        var input = Arrays.asList(fileContents.split("\\s+"));
+
+        int gamma = 0;
+        int epsilon = 0;
+
+        for (int i = 0; i < input.get(0).length(); i++) {
+            gamma *= 2;
+            epsilon *= 2;
+
+            if (mostOccurringNumber(input, i) == '1') {
+                // there were more ones than zeroes at this position
+                gamma++;
+            } else {
+                epsilon++;
+            }
+        }
+
+        System.out.println("Solution 1: " + gamma*epsilon);
+    }
+
+    private char mostOccurringNumber(List<String> binaryNumbers, int position) {
+        int count = 0;
+        int total = binaryNumbers.size();
+
+        for (var n : binaryNumbers) {
+            if (n.charAt(position) == '1') count++;
+        }
+
+        if (count >= (total+1)/2) return '1';
+        else return '0';
+    }
+
+    private String computeOxygen(List<String> strings, int currentPosition) {
+        char mostOccurring = mostOccurringNumber(strings, currentPosition);
+
+        var subresult = new ArrayList<String>();
+
+        for (var numbers : strings) {
+            if (numbers.charAt(currentPosition) == mostOccurring) subresult.add(numbers);
+        }
+
+        if (subresult.size() == 1) return subresult.get(0);
+        else return computeOxygen(subresult, currentPosition+1);
+    }
+
+    private String computeScrubbing(List<String> strings, int currentPosition) {
+        char mostOccurring = mostOccurringNumber(strings, currentPosition);
+
+        var subresult = new ArrayList<String>();
+
+        for (var numbers : strings) {
+            if (numbers.charAt(currentPosition) != mostOccurring) subresult.add(numbers);
+        }
+
+        if (subresult.size() == 1) return subresult.get(0);
+        else return computeScrubbing(subresult, currentPosition+1);
+    }
+
+    private int binaryToDecimal(String input) {
+        int res = 0;
+        for (var ch : input.toCharArray()) {
+            res *= 2;
+            if (ch == '1') res++;
+        }
+        return res;
+    }
+
+    @Override
+    protected void part2(String fileContents) {
+        var input = Arrays.asList(fileContents.split("\\s+"));
+
+        var oxygen = binaryToDecimal(computeOxygen(input, 0));
+        var scrubbing = binaryToDecimal(computeScrubbing(input, 0));
+
+        System.out.println("Solution of part 2: " + oxygen * scrubbing);
+    }
+
+    public static void main(String[] args) {
+        run(Day3::new, "example.txt", "input.txt");
+    }
+}
