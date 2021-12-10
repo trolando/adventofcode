f148109c2d0f0f1cd531b9b18b9ef7aa9aa3f982
Tom van Dijk <tom@tvandijk.nl>
2021-12-10 07:56:11 +0100

    Day 10

---
+++ src/nl/tvandijk/aoc/year2021/day10/Day10.java
@@ -0,0 +1,87 @@
+package nl.tvandijk.aoc.year2021.day10;
+
+import nl.tvandijk.aoc.common.Day;
+
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+
+public class Day10 extends Day {
+    /**
+     check if line is corrupted or incomplete
+     returns 0 if not either
+     returns -size if incomplete with how many are still needed
+     return positive number for syntax error score
+     */
+    long checkLine(String line) {
+        Deque<Character> chunks = new ArrayDeque<>();
+
+        for (int i = 0; i < line.length(); i++) {
+            var ch = line.charAt(i);
+            switch (ch) {
+                case '(','[', '{', '<' -> {
+                    chunks.add(ch);
+                }
+                case ')' -> {
+                    var other = chunks.removeLast();
+                    if (other != '(') return 3;
+                }
+                case ']' -> {
+                    var other = chunks.removeLast();
+                    if (other != '[') return 57;
+                }
+                case '}' -> {
+                    var other = chunks.removeLast();
+                    if (other != '{') return 1197;
+                }
+                case '>' -> {
+                    var other = chunks.removeLast();
+                    if (other != '<') return 25137;
+                }
+                default -> {
+                    throw new RuntimeException("unexpected character");
+                }
+            }
+        }
+
+        /*
+        ): 1 point.
+        ]: 2 points.
+        }: 3 points.
+        >: 4 points.
+         */
+
+        long result = 0;
+
+        while (!chunks.isEmpty()) {
+            result *= 5;
+            var ch = chunks.removeLast();
+            if (ch == '(') result += 1;
+            else if (ch == '[') result += 2;
+            else if (ch == '{') result += 3;
+            else if (ch == '<') result += 4;
+            else throw new RuntimeException("unexpected character");
+        }
+
+        return -1 * result;
+    }
+
+    @Override
+    protected void part1(String fileContents) {
+        var lines = fileContents.split("[\r\n]+");
+        var result = Arrays.stream(lines).mapToLong(this::checkLine).filter(x -> x>0).sum();
+        System.out.println("part 1 = " + result);
+    }
+
+    @Override
+    protected void part2(String fileContents) {
+        var lines = fileContents.split("[\r\n]+");
+        var result = Arrays.stream(lines).mapToLong(this::checkLine).filter(x -> x<0).sorted().toArray();
+        var median = -result[result.length/2];
+        System.out.println("part 2 = " + median);
+    }
+
+    public static void main(String[] args) {
+        run(Day10::new, "example.txt", "input.txt");
+    }
+}
