4e05a2561c6c1f59229d00ba62f33581df7f0535
Tom van Dijk <tom@tvandijk.nl>
2021-12-12 15:28:19 +0100

    Day 12 but faster

--- src/nl/tvandijk/aoc/year2021/day12/Day12.java
+++ src/nl/tvandijk/aoc/year2021/day12/Day12.java
@@ -2,83 +2,98 @@ package nl.tvandijk.aoc.year2021.day12;
 
 import nl.tvandijk.aoc.common.Day;
 
-import java.util.*;
+import java.util.ArrayList;
 
 public class Day12 extends Day {
-    private Map<String, Set<String>> parse(String fileContents) {
-        Map<String, Set<String>> edges = new HashMap<>();
+    private int locCount = 0;   // number of locations
+    private boolean[] edges;    // size locCount*locCount
+    private boolean[] isSmall;  // whether a location is small
+    private boolean[] visited;  // whether a location is visited in the current dfs
+    private long pathCount = 0; // number of distinct paths found
+
+    private void parse(String fileContents) {
+        var locs = new ArrayList<String>();
+        locs.add("start"); // encode start as 0
+        locs.add("end");   // encode end as 1
+
+        var edgelist = new ArrayList<Integer>();
+
         // parse each line
-        for (var line : fileContents.split("[\r\n]+")) {
+        var lines = fileContents.split("[\r\n]+");
+        for (int i = 0; i < lines.length; i++) {
             // split each line into two parts
-            var parts = line.split("-");
-            var from = parts[0];
-            var to = parts[1];
-            // from -> to
-            if (!to.equals("start")) {
-                edges.compute(from, (key, value) -> {
-                    if (value == null) value = new HashSet<>();
-                    value.add(to);
-                    return value;
-                });
+            var parts = lines[i].split("-");
+
+            int one = locs.indexOf(parts[0]);
+            if (one == -1) {
+                locs.add(parts[0]);
+                one = locs.size() - 1;
             }
-            // to -> from
-            if (!from.equals("start")) {
-                edges.compute(to, (key, value) -> {
-                    if (value == null) value = new HashSet<>();
-                    value.add(from);
-                    return value;
-                });
+
+            int two = locs.indexOf(parts[1]);
+            if (two == -1) {
+                locs.add(parts[1]);
+                two = locs.size() - 1;
             }
+
+            edgelist.add(one);
+            edgelist.add(two);
         }
-        return edges;
+
+        // initialize internal datastructures
+        locCount = locs.size();
+        isSmall = new boolean[locCount];
+        for (int i = 0; i < locCount; i++) {
+            isSmall[i] = Character.isLowerCase(locs.get(i).charAt(0));
+        }
+        edges = new boolean[locCount*locCount];
+        for (int i = 0; i < edgelist.size(); i+=2) {
+            int one = edgelist.get(i);
+            int two = edgelist.get(i+1);
+            edges[one*locCount+two] = true;
+            edges[two*locCount+one] = true;
+        }
+        visited = new boolean[locCount];
+        pathCount = 0;
     }
 
-    private void createPaths(Set<String> paths, List<String> currentPath, Map<String, Set<String>> edges, String allowTwice) {
-        var currentLocation = currentPath.get(currentPath.size()-1);
-        if (currentLocation.equals("end")) {
-            paths.add(String.join(",", currentPath));
-        } else {
-            for (String to : edges.get(currentLocation)) {
-                // check if already seen
-                if (Character.toLowerCase(to.charAt(0)) == to.charAt(0)) {
-                    var visitedTimes = currentPath.stream().filter(x -> x.equals(to)).count();
-                    int maxTimes = allowTwice.equals(to) ? 2 : 1;
-                    if (visitedTimes >= maxTimes) continue;
+    private void createPaths(int currentLocation, boolean allowTwice) {
+        for (int to = 1; to < locCount; to++) { // skip "start"
+            if (edges[currentLocation + to * locCount]) {
+                if (to == 1) {
+                    pathCount++;
+                } else {
+                    // check if already seen
+                    if (isSmall[to]) {
+                        if (visited[to]) {
+                            if (allowTwice) {
+                                createPaths(to, false);
+                            }
+                        } else {
+                            visited[to] = true;
+                            createPaths(to, allowTwice);
+                            visited[to] = false;
+                        }
+                    } else {
+                        createPaths(to, allowTwice);
+                    }
                 }
-                // if not, go there...
-                currentPath.add(to);
-                createPaths(paths, currentPath, edges, allowTwice);
-                currentPath.remove(currentPath.size() - 1);
             }
         }
     }
 
     @Override
     protected void part1(String fileContents) {
-        var edges = parse(fileContents);
-
-        Set<String> paths = new HashSet<>();
-        List<String> currentPath = new ArrayList<>();
-        currentPath.add("start");
-        createPaths(paths, currentPath, edges, "");
-
-        System.out.println("Part 1: " + paths.size());
+        parse(fileContents);
+        createPaths(0,false);
+        System.out.println("Part 1: " + pathCount);
     }
 
     @Override
     protected void part2(String fileContents) {
-        var edges = parse(fileContents);
-
-        Set<String> paths = new HashSet<>();
-        List<String> currentPath = new ArrayList<>();
-        currentPath.add("start");
-        for (var place : edges.keySet()) {
-            if (Character.toLowerCase(place.charAt(0)) == place.charAt(0)) {
-                createPaths(paths, currentPath, edges, place);
-            }
-        }
-
-        System.out.println("Part 2: " + paths.size());
+        parse(fileContents);
+        createPaths(0,true);
+        System.out.println("Part 2: " + pathCount);
     }
 
     public static void main(String[] args) {
