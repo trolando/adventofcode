4fc2c3e5774c1fcf15f0de4fa4d711d6bebb58a5
Tom van Dijk <t.vandijk@gmail.com>
2022-12-25 09:12:09 +0100

    And a bit of cleanup

--- src/nl/tvandijk/aoc/year2022/day25/Day25.java
+++ src/nl/tvandijk/aoc/year2022/day25/Day25.java
@@ -1,8 +1,6 @@
 package nl.tvandijk.aoc.year2022.day25;
 
-import java.util.*;
 import nl.tvandijk.aoc.common.Day;
-import nl.tvandijk.aoc.util.*;
 
 public class Day25 extends Day {
     @Override
@@ -18,38 +16,14 @@ public class Day25 extends Day {
         for (var line : lines) {
             long l = 0;
             for (var c : line.toCharArray()) {
-                l*=5;
-                if (c == '2') l += 2;
-                if (c == '1') l += 1;
-                if (c == '0') l += 0;
-                if (c == '-') l -= 1;
-                if (c == '=') l -= 2;
+                l = 5 * l + "=-0123".indexOf(c) - 2;
             }
-            System.out.printf("%s = %d%n", line, l);
-            sum+=l;
+            sum += l;
         }
-        System.out.println("sum = " + sum);
         String ans = "";
         while (sum != 0) {
-            var s = sum%5;
-            if (s == 0) ans = "0"+ans;
-            if (s == 1) {
-                ans = "1"+ans;
-                sum--;
-            }
-            if (s == 2) {
-                ans = "2"+ans;
-                sum -= 2;
-            }
-            if (s == 3) {
-                ans = "="+ans;
-                sum += 2;
-            }
-            if (s == 4) {
-                ans = "-"+ans;
-                sum += 1;
-            }
-            sum /= 5;
+            ans = "012=-".charAt((int) (sum % 5)) + ans;
+            sum = (sum + 2) / 5;
         }
         return ans;
     }
