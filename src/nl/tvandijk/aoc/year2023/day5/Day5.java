8c13dee5d30b90e8d68f80c8829d871cc4ed2872
Tom van Dijk <tom@tvandijk.nl>
2023-12-05 21:01:06 +0100

    Day 5 rewrite for fun

--- src/nl/tvandijk/aoc/year2023/day5/Day5.java
+++ src/nl/tvandijk/aoc/year2023/day5/Day5.java
@@ -1,95 +1,103 @@
 package nl.tvandijk.aoc.year2023.day5;
 
-import java.util.*;
-import java.util.regex.Pattern;
-import java.util.stream.Collectors;
-
 import nl.tvandijk.aoc.common.Day;
-import nl.tvandijk.aoc.util.*;
+import nl.tvandijk.aoc.util.Pair;
+import nl.tvandijk.aoc.util.Util;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Pattern;
 
 public class Day5 extends Day {
+    private List<Long> applyP1(List<String> map, List<Long> seeds) {
+        var filters = map.stream().skip(1).map(s -> Arrays.stream(s.split("\\s+")).mapToLong(Long::parseLong).toArray()).toList();
+        List<Long> next = new ArrayList<>();
+        for (var s : seeds) {
+            boolean done = false;
+            for (var l : filters) {
+                if (s >= l[1] && s < (l[1] + l[2])) {
+                    next.add(s - l[1] + l[0]);
+                    done = true;
+                    break;
+                }
+            }
+            if (!done) {
+                next.add(s);
+            }
+        }
+        return next;
+    }
+
+    private List<Pair<Long,Long>> applyP2(List<String> map, List<Pair<Long,Long>> ranges) {
+        var filters = map.stream().skip(1).map(s -> Arrays.stream(s.split("\\s+")).mapToLong(Long::parseLong).toArray()).toList();
+        var todo = new ArrayList<>(ranges);
+        List<Pair<Long,Long>> next = new ArrayList<>();
+        for (int j = 0; j < todo.size(); j++) {
+            var range = todo.get(j);
+            boolean done = false;
+            for (var ll : filters) {
+                if (range.a < ll[1]) {
+                    if ((range.a + range.b) > ll[1]) {
+                        // split
+                        long len = ll[1] - range.a;
+                        todo.add(Pair.of(range.a, len));
+                        todo.add(Pair.of(range.a + len, range.b - len));
+                        done = true;
+                    }
+                } else {
+                    // seed.a >= ll[1]
+                    if (range.a < (ll[1] + ll[2])) {
+                        if ((range.a + range.b) <= (ll[1] + ll[2])) {
+                            // seeds are contained in range
+                            next.add(Pair.of(range.a - ll[1] + ll[0], range.b));
+                            done = true;
+                        } else {
+                            // split
+                            long len = (ll[1] + ll[2]) - range.a;
+                            todo.add(Pair.of(range.a, len));
+                            todo.add(Pair.of(range.a + len, range.b - len));
+                            done = true;
+                        }
+                    }
+                }
+                if (done) break;
+            }
+            if (!done) {
+                next.add(range);
+            }
+        }
+        return next;
+    }
 
     @Override
     protected Object part1() {
         // part 1
-        ArrayList<Long> seeds = new ArrayList<>();
-        var matcher = Pattern.compile("\\d+").matcher(lines[0]);
-        while (matcher.find()) {
-            seeds.add(Long.parseLong(matcher.group(0)));
+        var l0seeds = lines[0].split(":")[1].trim().split("\\s+");
+        List<Long> seeds = new ArrayList<>(Arrays.stream(l0seeds).map(Long::parseLong).toList());
+
+        var lx = Util.splitArray(lines, String::isBlank);
+        for (int i = 1; i < lx.size(); i++) {
+            seeds = applyP1(lx.get(i), seeds);
         }
-        ArrayList<Long> next = new ArrayList<>();
-        for (int i = 2; i < lines.length; i++) {
-            var l = lines[i].trim();
-            if (l.isBlank()) {
-                next.addAll(seeds);
-                seeds = next;
-                next = new ArrayList<>();
-            } else if (Character.isDigit(l.charAt(0))) {
-                var ll = Arrays.stream(l.split("\\s+")).mapToLong(Long::parseLong).toArray();
-                var it = seeds.iterator();
-                while (it.hasNext()) {
-                    var seed = it.next();
-                    if (seed >= ll[1] && seed < (ll[1] + ll[2])) {
-                        // matches
-                        next.add(seed - ll[1] + ll[0]);
-                        it.remove();
-                    }
-                }
-            }
-        }
-        next.addAll(seeds);
-        seeds = next;
+
         return seeds.stream().mapToLong(Long::longValue).min().orElseThrow();
     }
 
     @Override
     protected Object part2() {
         // part 2
-        ArrayList<Pair<Long,Long>> ranges = new ArrayList<>();
+        List<Pair<Long,Long>> ranges = new ArrayList<>();
         var matcher = Pattern.compile("(\\d+)\\s+(\\d+)").matcher(lines[0]);
         while (matcher.find()) {
             ranges.add(Pair.of(Long.parseLong(matcher.group(1)), Long.parseLong(matcher.group(2))));
         }
-        ArrayList<Pair<Long,Long>> next = new ArrayList<>();
-        for (int i = 2; i < lines.length; i++) {
-            var l = lines[i].trim();
-            if (l.isBlank()) {
-                next.addAll(ranges);
-                ranges = next;
-                next = new ArrayList<>();
-            } else if (Character.isDigit(l.charAt(0))) {
-                var ll = Arrays.stream(l.split("\\s+")).mapToLong(Long::parseLong).toArray();
-                for (int j = 0; j < ranges.size(); j++) {
-                    var seed = ranges.get(j);
-                    if (seed.a < ll[1]) {
-                        if ((seed.a + seed.b) > ll[1]) {
-                            long len = ll[1] - seed.a;
-                            ranges.set(j, Pair.of(seed.a, len));
-                            ranges.add(j + 1, Pair.of(seed.a + len, seed.b - len));
-                            j--;
-                        }
-                    } else {
-                        // seed.a >= ll[1]
-                        if (seed.a < (ll[1] + ll[2])) {
-                            if ((seed.a + seed.b) <= (ll[1] + ll[2])) {
-                                // seeds are contained in range
-                                next.add(Pair.of(seed.a - ll[1] + ll[0], seed.b));
-                                ranges.remove(seed);
-                                j--;
-                            } else {
-                                // split
-                                long len = (ll[1] + ll[2]) - seed.a;
-                                ranges.set(j, Pair.of(seed.a, len));
-                                ranges.add(j + 1, Pair.of(seed.a + len, seed.b - len));
-                                j--;
-                            }
-                        }
-                    }
-                }
-            }
+
+        var lx = Util.splitArray(lines, String::isBlank);
+        for (int i = 1; i < lx.size(); i++) {
+            ranges = applyP2(lx.get(i), ranges);
         }
-        next.addAll(ranges);
-        ranges = next;
+
         return ranges.stream().mapToLong(s -> s.a).min().orElseThrow();
     }
 }
