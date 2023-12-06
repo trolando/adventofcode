b7ed17e79ba37a281d58454e1471a46ee332883d
Tom van Dijk <tom@tvandijk.nl>
2023-12-06 13:27:56 +0100

    Nicer Day 6

--- src/nl/tvandijk/aoc/year2023/day6/Day6.java
+++ src/nl/tvandijk/aoc/year2023/day6/Day6.java
@@ -1,8 +1,9 @@
 package nl.tvandijk.aoc.year2023.day6;
 
-import java.util.*;
 import nl.tvandijk.aoc.common.Day;
-import nl.tvandijk.aoc.util.*;
+
+import java.util.Arrays;
+import java.util.stream.LongStream;
 
 public class Day6 extends Day {
     @Override
@@ -10,15 +11,10 @@ public class Day6 extends Day {
         // part 1
         var t = Arrays.stream(lines[0].split("\\s+")).skip(1).mapToInt(Integer::parseInt).toArray();
         var d = Arrays.stream(lines[1].split("\\s+")).skip(1).mapToInt(Integer::parseInt).toArray();
-        int s = 1;
+        long s = 1;
         for (int i = 0; i < t.length; i++) {
-            int ways = 0;
-            for (int j = 0; j < t[i]; j++) {
-                int dist = j*(t[i]-j);
-                if (dist > d[i]) ways++;
-            }
-            s *= ways;
-
+            int k = i;
+            s *= LongStream.range(0, t[k]).parallel().map(x -> x*(t[k]-x)).filter(x -> x > d[k]).count();
         }
         return s;
     }
@@ -26,13 +22,8 @@ public class Day6 extends Day {
     @Override
     protected Object part2() {
         // part 2
-        var t = Long.parseLong(lines[0].split(":")[1].trim().replaceAll("\\s+", ""));
-        var d = Long.parseLong(lines[1].split(":")[1].trim().replaceAll("\\s+", ""));
-        long ways = 0;
-        for (int j = 0; j < t; j++) {
-            long dist = j*(t-j);
-            if (dist > d) ways++;
-        }
-        return ways;
+        var t = Long.parseLong(lines[0].split(": ")[1].replaceAll("\\s+", ""));
+        var d = Long.parseLong(lines[1].split(": ")[1].replaceAll("\\s+", ""));
+        return LongStream.range(0, t).parallel().map(i -> i*(t-i)).filter(i -> i > d).count();
     }
 }
