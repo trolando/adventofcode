5537bc37ad08dbcfbfddf25504b6d56e54daf99e
Tom van Dijk <tom@tvandijk.nl>
2023-12-12 08:45:55 +0100

    Day 12 some cleanup

--- src/nl/tvandijk/aoc/year2023/day12/Day12.java
+++ src/nl/tvandijk/aoc/year2023/day12/Day12.java
@@ -10,39 +10,39 @@ import java.util.Map;
 public class Day12 extends Day {
     private final Map<Pair<Integer, Integer>, Long> cache = new HashMap<>();
 
-    private long compute(String s, long[] numbers) {
-        cache.clear();
-        return compute(s, 0, numbers, 0);
-    }
-
-    private long compute(String s, int k, long[] numbers, int x) {
-        // check if we matched all
+    private long compute(String s, int k, int[] numbers, int x) {
+        // if all numbers are matched, check that there are no blocks left
         if (x == numbers.length) {
             for (int i = k; i < s.length(); i++) {
                 if (s.charAt(i) == '#') return 0L;
             }
             return 1L;
         }
+        // skip empty space until the next ? or #
+        while (s.length() > k && s.charAt(k) == '.') k++;
+        // check if there are enough characters left
         if (s.length() - k < numbers[x]) return 0L;
-        // ok we did not match all, so we need to match the next number
+        // enough characters left, consult the cache
         var key = Pair.of(k, x);
         if (cache.containsKey(key)) return cache.get(key);
+        // not in the cache, so first time to compute
         long res = 0;
-        // first try to match the next number...
+        // case 1: try to match the next block...
         boolean matches = true;
         for (int i = 0; i < numbers[x]; i++) {
-            if (s.length() <= k + i || s.charAt(k + i) == '.') {
+            if (s.charAt(k + i) == '.') {
                 matches = false;
                 break;
             }
         }
-        if (matches && (s.length() <= k + numbers[x] || s.charAt(k + (int) numbers[x]) != '#')) {
-            res += compute(s, k + (int) numbers[x] + 1, numbers, x + 1);
+        if (matches && (s.length() <= k + numbers[x] || s.charAt(k + numbers[x]) != '#')) {
+            res += compute(s, k + numbers[x] + 1, numbers, x + 1);
         }
-        // try to not match it
+        // case 2: skip the next character (unless forced #)
         if (s.charAt(k) != '#') {
             res += compute(s, k + 1, numbers, x);
         }
+        // cache the result and return
         cache.put(key, res);
         return res;
     }
@@ -53,8 +53,9 @@ public class Day12 extends Day {
         long sum = 0;
         for (var l : lines) {
             var parts = l.split(" ");
-            var numbers = Arrays.stream(parts[1].split(",")).mapToLong(Long::parseLong).toArray();
-            sum += compute(parts[0], numbers);
+            var numbers = Arrays.stream(parts[1].split(",")).mapToInt(Integer::parseInt).toArray();
+            cache.clear();
+            sum += compute(parts[0], 0, numbers, 0);
         }
         return sum;
     }
@@ -65,10 +66,11 @@ public class Day12 extends Day {
         long sum = 0;
         for (var l : lines) {
             var parts = l.split(" ");
-            String p0 = parts[0]+"?"+parts[0]+"?"+parts[0]+"?"+parts[0]+"?"+parts[0];
-            String p1 = parts[1]+","+parts[1]+","+parts[1]+","+parts[1]+","+parts[1];
-            var numbers = Arrays.stream(p1.split(",")).mapToLong(Long::parseLong).toArray();
-            sum += compute(p0, numbers);
+            String p0 = parts[0] + "?" + parts[0] + "?" + parts[0] + "?" + parts[0] + "?" + parts[0];
+            String p1 = parts[1] + "," + parts[1] + "," + parts[1] + "," + parts[1] + "," + parts[1];
+            var numbers = Arrays.stream(p1.split(",")).mapToInt(Integer::parseInt).toArray();
+            cache.clear();
+            sum += compute(p0, 0, numbers, 0);
         }
         return sum;
     }
